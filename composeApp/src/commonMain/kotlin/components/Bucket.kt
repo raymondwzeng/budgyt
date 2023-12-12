a6d40c924be5bb7ed8238870594aed6bf8a15ec8
raymondwzeng <rzeng3@gmail.com>
2023-12-11 20:59:44 -0600

    Changes to Bucket component to be really a container component

--- composeApp/src/commonMain/kotlin/components/Bucket.kt
+++ composeApp/src/commonMain/kotlin/components/Bucket.kt
@@ -1,6 +1,5 @@
 package components
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -14,6 +13,7 @@ import kotlinx.datetime.TimeZone
 import kotlinx.datetime.todayIn
 import models.Bucket
 import models.BucketType
+import models.Container
 import models.Transaction
 import java.util.UUID
 
@@ -40,27 +40,29 @@ val EXAMPLE_BUDGET_2 = listOf(
 val EXAMPLE_BUCKET = Bucket(
     id = UUID.randomUUID(),
     bucketName = "Example Bucket",
-    bucketType = BucketType.OUTFLOW,
     transactions = EXAMPLE_BUDGET
 )
 
 val EXAMPLE_BUCKET_2 = Bucket(
     id = UUID.randomUUID(),
     bucketName = "Example Bucket 2",
-    bucketType = BucketType.INFLOW,
     transactions = EXAMPLE_BUDGET_2
 )
 
-val EXAMPLE_BUCKET_LIST = listOf(EXAMPLE_BUCKET, EXAMPLE_BUCKET_2)
+val EXAMPLE_CONTAINER = Container(
+    containerType = BucketType.OUTFLOW,
+    buckets = listOf(EXAMPLE_BUCKET, EXAMPLE_BUCKET_2)
+)
+
+val EXAMPLE_CONTAINERS = listOf(EXAMPLE_CONTAINER)
 
 @Composable
-fun Bucket() {
-    //TODO: Parameters for text input/bucket type and everything else.
+fun Bucket() { //Really, this is a bucket of buckets.
     //This component should also hold the state for the inner internal items
-    LazyColumn { //TODO: Make sure that each category is only serviced once
-        items(EXAMPLE_BUCKET_LIST) { bucket ->
+    LazyColumn {
+        items(EXAMPLE_CONTAINERS) { container ->
             Text(
-                text = when(bucket.bucketType) {
+                text = when(container.containerType) {
                     BucketType.INFLOW -> "Inflow"
                     BucketType.OUTFLOW -> "Outflow"
                     BucketType.FUND -> "Fund"
@@ -68,14 +70,16 @@ fun Bucket() {
                 fontSize = 32.sp,
                 modifier = Modifier.padding(8.dp)
             )
-            BudgetCard(
-                name = bucket.bucketName,
-                estimatedAmount = 100f,
-                actualAmount = bucket.transactions.sumOf { it.transactionAmount.toDouble() },
-                onClick = fun() {
-                    println(bucket)
-                }
-            )
+            container.buckets.forEach { bucket ->
+                BudgetCard(
+                    name = bucket.bucketName,
+                    estimatedAmount = 100f,
+                    actualAmount = bucket.transactions.sumOf { it.transactionAmount.toDouble() },
+                    onClick = fun() {
+                        println(bucket)
+                    }
+                )
+            }
         }
     }
 }
