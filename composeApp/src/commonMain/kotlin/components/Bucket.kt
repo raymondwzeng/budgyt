cb72084882eb62de0d69d7506fa10f86f3940a96
raymondwzeng <rzeng3@gmail.com>
2023-12-11 21:06:00 -0600

    Added option for empty buckets to show all and display message

--- composeApp/src/commonMain/kotlin/components/Bucket.kt
+++ composeApp/src/commonMain/kotlin/components/Bucket.kt
@@ -1,11 +1,16 @@
 package components
 
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kotlinx.datetime.Clock
@@ -54,7 +59,17 @@ val EXAMPLE_CONTAINER = Container(
     buckets = listOf(EXAMPLE_BUCKET, EXAMPLE_BUCKET_2)
 )
 
-val EXAMPLE_CONTAINERS = listOf(EXAMPLE_CONTAINER)
+val EMPTY_INFLOW_CONTAINER = Container(
+    containerType = BucketType.INFLOW,
+    buckets = emptyList()
+)
+
+val EMPTY_FUND_CONTAINER = Container(
+    containerType = BucketType.FUND,
+    buckets = emptyList()
+)
+
+val EXAMPLE_CONTAINERS = listOf(EMPTY_INFLOW_CONTAINER, EXAMPLE_CONTAINER, EMPTY_FUND_CONTAINER)
 
 @Composable
 fun Bucket() { //Really, this is a bucket of buckets.
@@ -70,15 +85,22 @@ fun Bucket() { //Really, this is a bucket of buckets.
                 fontSize = 32.sp,
                 modifier = Modifier.padding(8.dp)
             )
-            container.buckets.forEach { bucket ->
-                BudgetCard(
-                    name = bucket.bucketName,
-                    estimatedAmount = 100f,
-                    actualAmount = bucket.transactions.sumOf { it.transactionAmount.toDouble() },
-                    onClick = fun() {
-                        println(bucket)
-                    }
-                )
+            if(container.buckets.isEmpty()) {
+                Card(modifier = Modifier.fillMaxWidth(0.8f)
+                    .padding(16.dp).height(IntrinsicSize.Min), elevation = 4.dp) {
+                    Text("No buckets for this category yet!", textAlign = TextAlign.Center, modifier = Modifier.padding(4.dp))
+                }
+            } else {
+                container.buckets.forEach { bucket ->
+                    BudgetCard(
+                        name = bucket.bucketName,
+                        estimatedAmount = 100f,
+                        actualAmount = bucket.transactions.sumOf { it.transactionAmount.toDouble() },
+                        onClick = fun() {
+                            println(bucket)
+                        }
+                    )
+                }
             }
         }
     }
