b04a6074e6a893212de10f94f35fdee2b8bf1153
raymondwzeng <rzeng3@gmail.com>
2023-12-13 22:30:38 -0600

    Working navigation

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,19 +1,22 @@
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.arkivanov.decompose.ComponentContext
-import components.Bucket
-import components.BudgetCard
+import com.arkivanov.decompose.extensions.compose.subscribeAsState
 import viewmodels.BaseViewModel
+import views.BucketsView
+import views.TransactionsView
 
 @Composable
 fun App(component: BaseViewModel) {
+    val child = component.callstack.subscribeAsState()
     MaterialTheme {
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Bucket()
+        Surface(modifier = Modifier.fillMaxSize()) {
+            when(child.value.active.instance) {
+                is BaseViewModel.Child.ListChild -> BucketsView(component = (child.value.active.instance as BaseViewModel.Child.ListChild).component)
+                is BaseViewModel.Child.DetailsChild -> TransactionsView(component = (child.value.active.instance as BaseViewModel.Child.DetailsChild).component)
+            }
         }
     }
 }
