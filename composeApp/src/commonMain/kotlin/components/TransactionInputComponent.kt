f847d8c24a93810a3a31fe90fe7a7d6afe6668e6
raymondwzeng <rzeng3@gmail.com>
2024-01-07 20:30:27 -0600

    Added TransactionInputComponent, renamed EditBucketView to be consistent

---
+++ composeApp/src/commonMain/kotlin/components/TransactionInputComponent.kt
@@ -0,0 +1,9 @@
+package components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun TransactionInputComponent(modifier: Modifier = Modifier, onInputChange: () -> Unit) {
+
+}
