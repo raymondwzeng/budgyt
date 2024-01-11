dba328ba1eed73c38a150576a087c64b6f62338a
raymondwzeng <raymondwzeng@github.com>
2024-01-10 21:41:29 -0800

    Begin migration of everything from Double to BigDecimal

--- shared/src/commonMain/kotlin/adapters/BigDecimalAdapter.kt
+++ shared/src/commonMain/kotlin/adapters/BigDecimalAdapter.kt
@@ -3,13 +3,13 @@ package adapters
 import app.cash.sqldelight.ColumnAdapter
 import java.math.BigDecimal
 
-val BigDecimalAdapter = object : ColumnAdapter<BigDecimal, Double> {
-    override fun decode(databaseValue: Double): BigDecimal {
+val BigDecimalAdapter = object : ColumnAdapter<BigDecimal, String> {
+    override fun decode(databaseValue: String): BigDecimal {
         return BigDecimal(databaseValue)
     }
 
-    override fun encode(value: BigDecimal): Double {
-        return BigDecimal(value).setScale(2, BigDecimal.ROUND_HALF_UP).toDouble()
+    override fun encode(value: BigDecimal): String {
+        return value.toString()
     }
 
 }
