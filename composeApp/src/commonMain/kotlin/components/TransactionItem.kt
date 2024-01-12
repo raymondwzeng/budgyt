2511ff16c0854f51fd9876c6290f33813f8b4045
raymondwzeng <raymondwzeng@github.com>
2024-01-12 15:05:57 -0800

    Refactored TransactionColorHelper, added colors to negative transactions

--- composeApp/src/commonMain/kotlin/components/TransactionItem.kt
+++ composeApp/src/commonMain/kotlin/components/TransactionItem.kt
@@ -1,14 +1,12 @@
 package components
 
 import GLOBAL_FORMATTER
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Card
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -17,8 +15,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import models.Transaction
-import java.text.NumberFormat
-import java.util.Locale
+import returnMonetaryValueColor
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
@@ -26,7 +23,7 @@ fun TransactionItem(modifier: Modifier = Modifier, transaction: Transaction, onC
     Card(modifier = modifier, elevation = 4.dp, onClick = onClick) {
         Column(modifier = Modifier.fillMaxSize().padding(4.dp)) {
             Text(text = transaction.transactionDate.toString(), fontSize = 12.sp, fontWeight = FontWeight.Light)
-            Text(text = GLOBAL_FORMATTER.format(transaction.transactionAmount), fontSize = 18.sp, textAlign = TextAlign.End)
+            Text(text = GLOBAL_FORMATTER.format(transaction.transactionAmount), fontSize = 18.sp, textAlign = TextAlign.End, color = returnMonetaryValueColor(value = transaction.transactionAmount, defaultPositiveColor = MaterialTheme.colors.onBackground))
             Text(text = transaction.note)
         }
     }
