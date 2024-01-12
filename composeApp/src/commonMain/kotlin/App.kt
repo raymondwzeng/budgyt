7433096ac1b2685de52b4b1a64419590c4fa9555
raymondwzeng <raymondwzeng@github.com>
2024-01-12 10:03:54 -0800

    Moved title/navigation to scaffold, plans for FAB

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,17 +1,22 @@
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.primarySurface
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.graphics.Color
 import com.arkivanov.decompose.extensions.compose.subscribeAsState
 import viewmodels.BaseViewModel
 import views.BucketView
@@ -25,16 +30,39 @@ enum class DeviceType {
     DESKTOP
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun App(deviceType: DeviceType, component: BaseViewModel) {
     val child = component.callstack.subscribeAsState()
     MaterialTheme {
-        Surface(modifier = Modifier.fillMaxSize()) {
+        Scaffold(
+            topBar = {
+                CenterAlignedTopAppBar(
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = MaterialTheme.colors.primarySurface,
+                        titleContentColor = MaterialTheme.colors.onPrimary
+                    ),
+                    title = { //TODO: Make this dynamic
+                        Text(text = "Budgyt")
+                    },
+                    navigationIcon = {
+                        IconButton(onClick = {
+                            component.onBackClicked(child.value.items.lastIndex - 1)
+                        }, enabled = child.value.items.size > 1) {
+                            Icon(
+                                Icons.Filled.ArrowBack,
+                                contentDescription = "Back",
+                                tint = if (child.value.items.size > 1) MaterialTheme.colors.onPrimary else Color.LightGray
+                            )
+                        }
+                    }
+                )
+            } //TODO: Floating action bar
+        ) { innerPadding ->
             Column(
-                modifier = Modifier.fillMaxSize(),
+                modifier = Modifier.fillMaxSize().padding(innerPadding),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text(text = "Budgyt", fontSize = 48.sp)
                 when (child.value.active.instance) {
                     is BaseViewModel.Child.ListChild -> ContainerView(component = (child.value.active.instance as BaseViewModel.Child.ListChild).component)
                     is BaseViewModel.Child.BucketDetailsChild -> BucketView(component = (child.value.active.instance as BaseViewModel.Child.BucketDetailsChild).component)
@@ -44,13 +72,6 @@ fun App(deviceType: DeviceType, component: BaseViewModel) {
                         component = (child.value.active.instance as BaseViewModel.Child.TransactionDetailsChild).component
                     )
                 }
-            }
-        }
-        if (deviceType == DeviceType.DESKTOP) {
-            IconButton(onClick = {
-                component.onBackClicked(child.value.items.lastIndex - 1)
-            }, enabled = child.value.items.size > 1) {
-                Icon(Icons.Filled.ArrowBack, contentDescription = "Back")
             }
         }
     }
